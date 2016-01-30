4694cbad907784ba18e49931f61d19ebd43d4d43
Emrlay <Emrlay@zjlin@live.com>
2016-01-30 14:47:42 +0800

    First Commit

---
+++ ONESim/input/EventQueue.java
@@ -0,0 +1,33 @@
+/* 
+ * Copyright 2010 Aalto University, ComNet
+ * Released under GPLv3. See LICENSE.txt for details. 
+ */
+package input;
+
+/**
+ * Interface for event queues. Any class that is not a movement model or a 
+ * routing module but wishes to provide events for the simulation (like creating
+ * messages) must implement this interface and register itself to the 
+ * simulator. See the {@link EventQueueHandler} class for configuration 
+ * instructions.
+ * 
+ * 事件队列接口；任何非移动模型及Routing的模型，但却希望为Simulation过程提供事件的类都应该
+ * 实现这一接口，并将自己注册给Simulator
+ */
+public interface EventQueue {
+	
+	/**
+	 * Returns the next event in the queue or ExternalEvent with time of 
+	 * double.MAX_VALUE if there are no events left.
+	 * @return The next event
+	 */
+	public ExternalEvent nextEvent();
+	
+	/**
+	 * Returns next event's time or Double.MAX_VALUE if there are no 
+	 * events left in the queue.
+	 * @return Next event's time
+	 */
+	public double nextEventsTime();
+
+}
